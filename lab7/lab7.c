a3163dbf76dbe66b57a4e5f240e4706f1d451208
artur_amaral <artur.amaral@mettainnovations.com.br>
2024-10-10 14:33:01 -0300

    Organiza código do lab7

--- lab7/lab7.c
+++ lab7/lab7.c
@@ -14,13 +14,14 @@
 
 // Variaveis globais
 sem_t podeEscreverNoBuffer, podeLerBuffer;      //semaforos para coordenar a ordem de execucao das threads
-int done = 0;
+int terminouLeituraDoArquivo = 0;
 
 char buffer1[N + 1];  // +1 to store the null terminator for the string
 
 //funcao executada pela thread 1
-void *leitor (void *arg) {
-   printf("[ t1 ] Leitor esta executando...\n");
+void *leitor (void *arg) 
+{
+    printf("[ t1 ] Leitor esta executando...\n");
    
     FILE *file;
     size_t bytesRead;
@@ -38,23 +39,26 @@ void *leitor (void *arg) {
         sem_wait( &podeEscreverNoBuffer ); // Se puder escrever no buffer1, prossegue.
         printf("[ t1 ] Leitor pode escrever no buffer1.\n");
 
-        // Clear the buffer before reading (set to all zeros)
+        // Reseta conteúdo do buffer a cada escrita.
+        // Resolve problema da última iteração quando o tamanho do arquivo não é múltiplo de N.
         memset(buffer1, 0, sizeof(buffer1));
-        // Read the file in chunks of N characters
+        
+        // Lê N blocos do arquivo.
         bytesRead = fread(buffer1, sizeof(char), N, file);
 
         printf("[ t1 ] buffer1: %s\n", buffer1);
 
+        // Verifica se chegou ao final do arquivo.
         if ( feof(file) ) 
         {
-            printf("DONE\n");
-            done = 1;  // Set the done flag when reading is finished
-            sem_post(&podeLerBuffer);  // Notify processador for final check
+            terminouLeituraDoArquivo = 1;
+            sem_post(&podeLerBuffer);
             break;
         }
 
+        // Avisa que o buffer1 foi preenchido e pode ser lido.   
         printf("[ t1 ] Processador pode consumir buffer1.\n");
-        sem_post( &podeLerBuffer );    // Avisa que o buffer1 foi preenchido e pode ser lido.   
+        sem_post( &podeLerBuffer );
     }
 
     fclose(file);
@@ -71,23 +75,26 @@ void *processador (void *arg) {
 
     while( 1 )
     {
+        // Quando o processador puder ler o buffer 1, prossegue
         printf("[ t2 ] Processador está aguardando liberação para consumir buffer1.\n");
-        sem_wait( &podeLerBuffer ); // Quando o processador puder ler o buffer 1, prossegue
+        sem_wait( &podeLerBuffer );
 
-        if ( done )
+        // Se detectar que o arquivo chegou ao final, quebra o loop.
+        if ( terminouLeituraDoArquivo )
         {
             printf("[ t2 ] Leitor terminou. Processador finalizando...\n");
             break;
         }
 
         printf("[ t2 ] Processador LIBERADO para consumir buffer1.\n");
-        // printf("%s\n", buffer1);
-        // printf(" len: %ld \n", strlen(buffer1));
-        strcpy(myBuffer1, buffer1);
 
+        // Copia conteúdo do buffer1 para buffer local.
+        strcpy(myBuffer1, buffer1);
         printf("[ t2 ] Processador criou cópia local do buffer1: %s\n", myBuffer1);
+
+        // Quando o processador já tiver lido o buffer e o leitor puder sobrescrevê-lo, emite este sinal.
         printf("[ t2 ] Leitor pode sobrescrever buffer1.\n");
-        sem_post( &podeEscreverNoBuffer); // Quando o processador já tiver lido o buffer e o leitor puder sobrescrevê-lo, emite este sinal.
+        sem_post( &podeEscreverNoBuffer);
         
     }
 
@@ -111,27 +118,22 @@ void *escritor (void *arg) {
     pthread_exit(NULL);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
   pthread_t tid[NTHREADS];
 
-  //inicia os semaforos
+  // inicia os semaforos
   sem_init(&podeEscreverNoBuffer, 0, 1);
   sem_init(&podeLerBuffer, 0, 0);
 
-  //cria as tres threads
-  if (pthread_create(&tid[2], NULL, leitor, NULL)) { 
-	  printf("--ERRO: pthread_create()\n"); exit(-1); }
-  if (pthread_create(&tid[1], NULL, processador, NULL)) { 
-	  printf("--ERRO: pthread_create()\n"); exit(-1); }
-  if (pthread_create(&tid[0], NULL, escritor, NULL)) { 
-	  printf("--ERRO: pthread_create()\n"); exit(-1); }
+  // cria as tres threads
+  if ( pthread_create( &tid[2], NULL, leitor, NULL ) ) { printf("--ERRO: pthread_create()\n"); exit(-1); }
+  if ( pthread_create( &tid[1], NULL, processador, NULL ) ) { printf("--ERRO: pthread_create()\n"); exit(-1); }
+  if  (pthread_create( &tid[0], NULL, escritor, NULL) ) { printf("--ERRO: pthread_create()\n"); exit(-1); }
 
   //--espera todas as threads terminarem
-  for (int t=0; t<NTHREADS; t++) {
-    if (pthread_join(tid[t], NULL)) {
-         printf("--ERRO: pthread_join() \n"); exit(-1); 
-    } 
-  } 
+  for (int t=0; t<NTHREADS; t++) 
+    if (pthread_join(tid[t], NULL)) { printf("--ERRO: pthread_join() \n"); exit(-1); }  
 
   return 0;
 }
